cb092a7a5b8fe7703df414ee5e2ec7961c8e6183
Yana <YanaLSoltan@gmail.com>
2018-02-27 22:14:05 +0300

    task 6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
-  //module.exports =
-    function check(str, bracketsConfig) {
+//module.exports =
+
+function check2(str, bracketsConfig) {
+    var stack = [];
+    var arrElements = str.split("");
+
+    for (var i = 0; i < arrElements.length; i++){
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (arrElements[i] === bracketsConfig[j][0]){
+                stack.push(arrElements[i]);
+            }
+            else if((arrElements[i] === bracketsConfig[j][1])){
+                stack.pop();
+            }
+
+        }
+    }
+
+    return stack.length === 0;
 
 }
 
-
-const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
 const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
 
 
-console.log(check('()', config1));
+//console.log(check('()', config1));
 //console.log(check('((()))()', config1));
-//console.log(check('())(', config1));
-
-}
+console.log(check2('([{}])', config3));
