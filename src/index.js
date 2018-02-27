3761614adeecac0230791a2e8a51cddc11f23a68
Yana <YanaLSoltan@gmail.com>
2018-02-27 21:14:38 +0300

    task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //module.exports =
+    function check(str, bracketsConfig) {
+
+}
+
+
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+
+
+console.log(check('()', config1));
+//console.log(check('((()))()', config1));
+//console.log(check('())(', config1));
+
 }
